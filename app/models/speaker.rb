f4992778bfde41dead284b0787ca3d4ada424a08
Jim Remsik <jim@bendyworks.com>
2013-01-06 23:19:53 -0600

    Randomize speakers on home page

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -3,4 +3,11 @@ class Speaker < ActiveRecord::Base
 
   validates_presence_of :name
   validates_presence_of :bio
+
+  module Scopes
+    def in_random_order
+      with_exclusive_scope{ order('RANDOM()') }
+    end
+  end
+  extend Scopes
 end
