e7a552c5370a82c0c32c313bbeb9bbc9cdc09497
Jim Remsik <jim@bendyworks.com>
2013-01-22 19:21:49 -0600

    Add day scopes to Talk model

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -2,4 +2,17 @@ class Talk < ActiveRecord::Base
   attr_accessible :description, :plenary, :speaker, :start, :title
 
   validates_presence_of :title, :start
+
+  module Scopes
+    def on_friday
+      where('start >= ?', Time.local('2013', 'feb', '15'))
+        .where('start <= ?', Time.local('2013', 'feb', '16'))
+    end
+
+    def on_saturday
+      where('start >= ?', Time.local('2013', 'feb', '16'))
+        .where('start <= ?', Time.local('2013', 'feb', '17'))
+    end
+  end
+  extend Scopes
 end
