68cff7ea49df281d526c43154b221908220bfdef
Jim Remsik <jim@bendyworks.com>
2013-01-22 21:21:39 -0600

    Sorting, that may be important

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -7,11 +7,13 @@ class Talk < ActiveRecord::Base
     def on_friday
       where('start >= ?', Time.local('2013', 'feb', '15'))
         .where('start <= ?', Time.local('2013', 'feb', '16'))
+        .order('start ASC')
     end
 
     def on_saturday
       where('start >= ?', Time.local('2013', 'feb', '16'))
         .where('start <= ?', Time.local('2013', 'feb', '17'))
+        .order('start ASC')
     end
   end
   extend Scopes
